45afa17de0d31cb75afd2df4a71325237e89fc6d
Katia Nienow <nienowkatia@gmail.com>
2023-10-05 16:13:00 -0300

    requisito 01 - ok

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,36 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient_1 = Ingredient("manteiga")
+    ingredient_2 = Ingredient("salmão")
+    ingredient_3 = Ingredient("massa de lasanha")
+    ingredient_lactose = Ingredient("manteiga")
+
+    assert ingredient_1 == ingredient_lactose
+    assert ingredient_1 != ingredient_2
+    assert ingredient_2 != ingredient_3
+    assert ingredient_3 != ingredient_1
+    assert ingredient_lactose.name == "manteiga"
+    assert ingredient_1.restrictions == {
+        Restriction.LACTOSE,
+        Restriction.ANIMAL_DERIVED
+    }
+    assert ingredient_2.restrictions == {
+        Restriction.ANIMAL_MEAT,
+        Restriction.SEAFOOD,
+        Restriction.ANIMAL_DERIVED,
+    }
+    assert ingredient_3.restrictions == {
+        Restriction.LACTOSE,
+        Restriction.GLUTEN
+    }
+    assert repr(ingredient_lactose) == "Ingredient('manteiga')"
+    assert hash(ingredient_1) == hash(ingredient_lactose)
+    assert hash(ingredient_1) != hash(ingredient_2)
+    assert hash(ingredient_2) != hash(ingredient_3)
+    assert hash(ingredient_3) != hash(ingredient_1)
+
+
+
