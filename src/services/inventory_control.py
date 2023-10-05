9528688257ea1e759b3c51383d9eeef1fc03913b
Katia Nienow <nienowkatia@gmail.com>
2023-10-05 20:09:52 -0300

    refatorando

--- src/services/inventory_control.py
+++ src/services/inventory_control.py
@@ -29,8 +29,8 @@ class InventoryMapping:
     def check_recipe_availability(self, recipe: Recipe) -> bool:
         try:
             return all(
-                    self.inventory[ingredient] >= recipe[ingredient]
-                    for ingredient in recipe.keys()
+                self.inventory[ingredient] >= recipe[ingredient]
+                for ingredient in recipe.keys()
             )
         except KeyError:
             return False
